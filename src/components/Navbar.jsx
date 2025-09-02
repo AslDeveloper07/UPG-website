dd83a8522e4b724701cfcec9fa84c576b22b2d8f
Asilbek Suvonov <aslwexking777@gmail.com>
2025-09-02 16:40:38 +0500

    adda

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-  return (
-<nav></nav>
-  )
-}
+  return <nav>
+    <div>
+      
+    </div>
+  </nav>;
+};
 
-export default Navbar
+export default Navbar;
