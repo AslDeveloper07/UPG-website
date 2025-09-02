1fd4f2fb26d940671c10c537be8c2193a2b2cbb7
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-09-02 16:53:20 +0500

    create header container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+import { header } from "framer-motion/client";
 import React from "react";
 
 const Navbar = () => {
-5
+  return (
+    <header className="w-full">
+      <nav className="container mx-auto bg-amber-500 h-20">
+        
+      </nav>
+    </header>
+  )
 };
 
 export default Navbar;
