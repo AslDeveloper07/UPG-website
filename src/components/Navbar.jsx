fe70cc19610880f796d186c005e89161dff2dff2
Asilbek Suvonov <aslwexking777@gmail.com>
2025-09-02 17:42:11 +0500

    addad

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,42 @@ import { IoWallet } from "react-icons/io5";
 import { MdFavoriteBorder } from "react-icons/md";
 import { PiShoppingCartDuotone } from "react-icons/pi";
 import { RiMessage2Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+const headerLink = [
+  {
+    icon: <IoWallet className="" />,
+    title: "uzs/usd"
+  }
+]
 
 const Navbar = () => {
+
+
   return (
     <header className="w-full bg-white dark:bg-black">
       <nav className="container mx-auto  h-20 flex  justify-between items-center">
-        <img src="" alt="" />
+        {/* <img src="" alt="" /> */}
 
         <input type="search" />
-        <div>
-          <div className="text-white">
-            <IoWallet className="text-white" />
-            <p>uzs/usd</p>
+        <div className="flex ">
+         <ul>
+          {
+            headerLink.map((item, i)=>(
+              <div key={i}>
+                <li>{item.icon}</li>
+                <li>{item.title}</li>
+              </div>
+            ))
+          }
+         </ul>
+          {/* <Link to="/wallet">
+           <div className="">
+            <IoWallet className="" />
+            <p></p>
           </div>
+          </Link> */}
+
           <div>
             <IoMdStats />
             <p>Сравненение</p>
