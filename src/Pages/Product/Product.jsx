37f2dfffc388dacc8cb181192a7b6824800cd715
Asilbek Suvonov <aslwexking777@gmail.com>
2025-09-08 23:27:56 +0500

    adadd

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,27 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import ProductImages from "../../components/Products/ProductImages";
-import ProductInfo from "../../components/Products/ProductInfo";
-import ProductTabs from "../../components/Products/ProductTabs";
-import RelatedProducts from "../../components/Products/RelatedProducts";
-import { gamerProducts } from "../../data/Data";
 
 const Product = () => {
-  const { id } = useParams();
-  const product = gamerProducts.find((item) => item.id === Number(id));
-
-  if (!product) return <p className="text-white">Mahsulot topilmadi</p>;
-
-  return (
-    <div className="p-10">
-      <div className="flex gap-10">
-        <ProductImages images={product.images} />
-        <ProductInfo product={product} />
-      </div>
-      <ProductTabs product={product} />
-      <RelatedProducts />
-    </div>
-  );
+  return <main className="bg-white dark:bg-[#111] w-full h-[800px]"></main>;
 };
 
 export default Product;
--- src/components/Products/ProductDescription.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProductDescription = ({ description }) => {
-  return <p className="mt-4">{description}</p>;
-};
-
-export default ProductDescription;
